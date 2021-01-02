87fc4b56e8bfd5ec22fc7cc35ae70776fed6afd7
Dhruv Jha <jhadhruv206@gmail.com>
2021-01-03 00:19:47 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+
+    // When email link is pressed then open mail client
+    $('#email-btn').click(function () {
+        window.open('mailto:jasminejerry@yahoo.in?subject=Your subject');
+    });
+
+    /**
+     *
+     * Handles the animation for different elements
+     *
+     */
+     
+
+})
