3781295099aad7bee24ebe7b4b40b0a2ed337f65
DHRUV KUMAR JHA <59375280+dhruvjha206@users.noreply.github.com>
2021-02-21 02:30:21 +0530

    Updated mail id

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
 
     // When email link is pressed then open mail client
     $('#email-btn').click(function () {
-        window.open('mailto:jasminejerry@yahoo.in?subject=Your subject');
+        window.open('mailto:dhruvjha.191me128@nitk.edu.in?subject=Your subject');
     });
 })
